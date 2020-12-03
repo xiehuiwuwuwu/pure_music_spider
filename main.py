78b4589f84631e95c7545fc83c300cb1f5a255c4
xiehui <1284417307@qq.com>
2020-12-03 23:42:37 +0800

    function-test pass

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def spider():		#按页数抓取-基本流程
     
     headers = {'user-agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36'}   #linux下谷歌浏览器请求头
 
-    for k in range(20):			#前20页   !!!改进!!!
+    for k in range(30):			#前20页   !!!改进!!!
         k                = str(k)
         url              = "https://www.hifini.com/index-" + k + ".htm"   #网站前20页的网址
         strr             = url_2_strr(url,headers)
@@ -64,9 +64,9 @@ def spider_type_chinese(): 		#按类别抓取-华语歌曲
 
     headers        = {'user-agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36'}   #linux下谷歌浏览器请求头
 
-    for page in range(1000):
+    for page in range(30):
         page = str(page)
-        chinese_url = "https://www.hifini.com/forum-1-" + page + ".htm?orderby=lastpid" 
+        chinese_url = "https://www.hifini.com/forum-1-" + page + ".htm?orderby=tid" 
 
     #chinese_url    = "https://www.hifini.com/forum-1-1.htm?orderby=lastpid"        #华语类别第一页url
         chinese_strr   = url_2_strr(chinese_url,headers)
@@ -112,7 +112,7 @@ def spider_type_chinese(): 		#按类别抓取-华语歌曲
                 print("!!!error to commit " + song_name + "!!!")
                 repeatnumber += 1
             print()
-        if repeatnumber > 18:
+        if repeatnumber > 23:
            # con.close()
             return
 
@@ -121,9 +121,9 @@ def spider_type_chinese(): 		#按类别抓取-华语歌曲
 def spider_type_JapAndKor():
     headers           = {'user-agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36'}
 
-    for page in range(120):
+    for page in range(30):
         page = str(page)
-        japAndkor_url     = "https://www.hifini.com/forum-15-" + page + ".htm?orderby=lastpid"
+        japAndkor_url     = "https://www.hifini.com/forum-15-" + page + ".htm?orderby=tid"
         japAndkor_strr    = url_2_strr(japAndkor_url,headers)
         japAndkor_songid  = get_songid('thread-([1-9][0-9][0-9]*)', japAndkor_strr)
         repeatnumber = 0
@@ -166,6 +166,200 @@ def spider_type_JapAndKor():
             return
 
 
+
+def spider_type_eurAndUs():
+    headers = {'user-agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36'}
+    for page in range(30):
+        page = str(page)
+        eurAndUs_url     = "https://www.hifini.com/forum-10-" + page + ".htm?orderby=tid"
+        eurAndUs_strr    = url_2_strr(eurAndUs_url,headers)
+        eurAndUs_songid  = get_songid('thread-([1-9][0-9][0-9]*)', eurAndUs_strr)
+        repeatnumber = 0
+        for i in eurAndUs_songid:
+            song_url   = "https://www.hifini.com/thread-" + i + ".htm"
+            song_strr  = url_2_strr(song_url,headers)
+            songName   = get_target(' title: \'(.*?)\',',song_strr)                     #可能出现空songname的情况-有id找不到name--bug
+            songAuthor = get_target(' author:\'(.*?)\',',song_strr)
+            songPic    = get_target(' pic: \'(.*?)\'',song_strr)
+
+            if len(songName):           #剔除无效结果
+                pass
+            else:
+                continue
+
+            songtype    = "欧美"
+            songtypenum = "2"                        #定义类别协议  ： 华语 = 0   日韩 = 1   欧美 = 2
+            song_name   = "".join(songName)
+            song_author = "".join(songAuthor)
+            song_pic    = "".join(songPic)
+
+            print("ID = " + i)                                #提供调试打印
+            print("".join(songName))
+            print("".join(songAuthor))
+            print("".join(songPic))
+            print(songtype)
+
+            j = int(i)
+
+            try:                                   #添加异常抛出   执行数据库的数据插入
+                cur.execute("INSERT INTO music_table(music_id,song_name,author,pic_url,type) VALUES(%s,%s,%s,%s,%s)",(j,song_name,song_author,song_pic,songtypenum))
+                con.commit()                  #执行完必须提交
+                print("success to commit " + song_name)
+            except:
+                print("!!!error to commit " + song_name + "!!!")
+                repeatnumber += 1
+            print()
+
+        if repeatnumber > 18:
+
+            return
+
+def spider_type_Remix():
+    headers = {'user-agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36'}
+    for page in range(30):
+        page = str(page)
+        remix_url     = "https://www.hifini.com/forum-11-" + page + ".htm?orderby=tid"
+        remix_strr    = url_2_strr(remix_url,headers)
+        remix_songid  = get_songid('thread-([1-9][0-9][0-9]*)', remix_strr)
+        repeatnumber = 0
+        for i in remix_songid:
+            song_url   = "https://www.hifini.com/thread-" + i + ".htm"
+            song_strr  = url_2_strr(song_url,headers)
+            songName   = get_target(' title: \'(.*?)\',',song_strr)                     #可能出现空songname的情况-有id找不到name--bug
+            songAuthor = get_target(' author:\'(.*?)\',',song_strr)
+            songPic    = get_target(' pic: \'(.*?)\'',song_strr)
+
+            if len(songName):           #剔除无效结果
+                pass
+            else:
+                continue
+
+            songtype    = "remix"
+            songtypenum = "3"                        #定义类别协议  ： 华语 = 0   日韩 = 1   欧美 = 2   remix = 3
+            song_name   = "".join(songName)
+            song_author = "".join(songAuthor)
+            song_pic    = "".join(songPic)
+
+            print("ID = " + i)                                #提供调试打印
+            print("".join(songName))
+            print("".join(songAuthor))
+            print("".join(songPic))
+            print(songtype)
+
+            j = int(i)
+
+            try:                                   #添加异常抛出   执行数据库的数据插入
+                cur.execute("INSERT INTO music_table(music_id,song_name,author,pic_url,type) VALUES(%s,%s,%s,%s,%s)",(j,song_name,song_author,song_pic,songtypenum))
+                con.commit()                  #执行完必须提交
+                print("success to commit " + song_name)
+            except:
+                print("!!!error to commit " + song_name + "!!!")
+                repeatnumber += 1
+            print()
+            
+        if repeatnumber > 18:
+
+            return
+
+def spider_type_pure():
+    headers = {'user-agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36'}
+    for page in range(30):
+        page = str(page)
+        pure_url     = "https://www.hifini.com/forum-12-" + page + ".htm?orderby=tid"
+        pure_strr    = url_2_strr(pure_url,headers)
+        pure_songid  = get_songid('thread-([1-9][0-9][0-9]*)', pure_strr)
+        repeatnumber = 0
+        for i in pure_songid:
+            song_url   = "https://www.hifini.com/thread-" + i + ".htm"
+            song_strr  = url_2_strr(song_url,headers)
+            songName   = get_target(' title: \'(.*?)\',',song_strr)                     #可能出现空songname的情况-有id找不到name--bug
+            songAuthor = get_target(' author:\'(.*?)\',',song_strr)
+            songPic    = get_target(' pic: \'(.*?)\'',song_strr)
+
+            if len(songName):           #剔除无效结果
+                pass
+            else:
+                continue
+
+            songtype    = "pure"
+            songtypenum = "4"                        #定义类别协议  ： 华语 = 0   日韩 = 1   欧美 = 2   remix = 3   纯音乐 = 4
+            song_name   = "".join(songName)
+            song_author = "".join(songAuthor)
+            song_pic    = "".join(songPic)
+
+            print("ID = " + i)                                #提供调试打印
+            print("".join(songName))
+            print("".join(songAuthor))
+            print("".join(songPic))
+            print(songtype)
+
+            j = int(i)
+
+            try:                                   #添加异常抛出   执行数据库的数据插入
+                cur.execute("INSERT INTO music_table(music_id,song_name,author,pic_url,type) VALUES(%s,%s,%s,%s,%s)",(j,song_name,song_author,song_pic,songtypenum))
+                con.commit()                  #执行完必须提交
+                print("success to commit " + song_name)
+            except:
+                print("!!!error to commit " + song_name + "!!!")
+                repeatnumber += 1
+            print()
+            
+        if repeatnumber > 18:
+
+            return
+
+def spider_type_different():
+    headers = {'user-agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36'}
+    for page in range(30):
+        page = str(page)
+        different_url     = "https://www.hifini.com/forum-13-" + page + ".htm?orderby=tid"
+        different_strr    = url_2_strr(different_url,headers)
+        different_songid  = get_songid('thread-([1-9][0-9][0-9]*)', different_strr)
+        repeatnumber = 0
+        for i in different_songid:
+            song_url   = "https://www.hifini.com/thread-" + i + ".htm"
+            song_strr  = url_2_strr(song_url,headers)
+            songName   = get_target(' title: \'(.*?)\',',song_strr)                     #可能出现空songname的情况-有id找不到name--bug
+            songAuthor = get_target(' author:\'(.*?)\',',song_strr)
+            songPic    = get_target(' pic: \'(.*?)\'',song_strr)
+
+            if len(songName):           #剔除无效结果
+                pass
+            else:
+                continue
+
+            songtype    = "different"
+            songtypenum = "5"                        #定义类别协议  ： 华语 = 0   日韩 = 1   欧美 = 2   remix = 3   纯音乐 = 4    different = 5
+            song_name   = "".join(songName)
+            song_author = "".join(songAuthor)
+            song_pic    = "".join(songPic)
+
+            print("ID = " + i)                                #提供调试打印
+            print("".join(songName))
+            print("".join(songAuthor))
+            print("".join(songPic))
+            print(songtype)
+
+            j = int(i)
+
+            try:                                   #添加异常抛出   执行数据库的数据插入
+                cur.execute("INSERT INTO music_table(music_id,song_name,author,pic_url,type) VALUES(%s,%s,%s,%s,%s)",(j,song_name,song_author,song_pic,songtypenum))
+                con.commit()                  #执行完必须提交
+                print("success to commit " + song_name)
+            except:
+                print("!!!error to commit " + song_name + "!!!")
+                repeatnumber += 1
+            print()
+            
+        if repeatnumber > 18:
+
+            return
+
 if __name__ == '__main__':
-    spider_type_chinese()   
+    spider_type_chinese()
+    spider_type_JapAndKor()   
+    spider_type_eurAndUs()
+    spider_type_Remix()
+    spider_type_pure()
+    spider_type_different()
     con.close()
