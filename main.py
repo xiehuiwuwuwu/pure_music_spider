708e8b9e77bca060bc53340d61067f1f0f1726fc
xiehui <1284417307@qq.com>
2020-12-05 16:50:56 +0800

    createlogfile

--- main.py
+++ main.py
@@ -5,11 +5,27 @@ import re
 import time
 import datetime
 import pymysql
+import threading
+import random
+from enum import Enum
+import traceback
+import sys
+
+class Typename(Enum):
+    CHINESE = 1
+    JAPANDKORE = 15
+    EURANDUSE = 10
+    REMIXE = 11
+    PURE = 12
+    DIFFERENTE = 13 
 
 con            = pymysql.connect(host = '8.131.54.184', user = 'root', passwd = '609597441@GHQq', charset = 'utf8')             #连接数据库
 cur            = con.cursor()                           #获取游标
 print("connection successful！")                  #连接成功提示
 cur.execute("use pure_music;")                     #使用库中pure_music表
+lock = threading.Lock()
+
+user_agent = [  "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1"  ,  "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6",    "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1",    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5",    "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5",    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",    "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",    "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"]
 
 
 def url_2_strr(s,header=''):		#url网址转换成文本格式   #添加请求头header防止初步反爬
@@ -62,9 +78,11 @@ def spider():		#按页数抓取-基本流程
 
 def spider_type_chinese(): 		#按类别抓取-华语歌曲
 
-    headers        = {'user-agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36'}   #linux下谷歌浏览器请求头
+    #headers        = {'user-agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36'}   #linux下谷歌浏览器请求头
 
     for page in range(30):
+        headers = { "User-Agent": random.choice(user_agent)}
+
         page = str(page)
         chinese_url = "https://www.hifini.com/forum-1-" + page + ".htm?orderby=tid" 
 
@@ -89,6 +107,10 @@ def spider_type_chinese(): 		#按类别抓取-华语歌曲
                 pass
             else:
                 continue
+            if len(songAuthor):
+                pass
+            else:
+                continue
 
             songtype    = "华语"
             songtypenum = "0"                        #定义类别协议  ： 华语 = 0
@@ -104,6 +126,7 @@ def spider_type_chinese(): 		#按类别抓取-华语歌曲
 
             j = int(i)
 
+            lock.acquire()
             try:                                   #添加异常抛出   执行数据库的数据插入
                 cur.execute("INSERT INTO music_table(music_id,song_name,author,pic_url,type) VALUES(%s,%s,%s,%s,%s)",(j,song_name,song_author,song_pic,songtypenum))
                 con.commit()                  #执行完必须提交
@@ -111,7 +134,9 @@ def spider_type_chinese(): 		#按类别抓取-华语歌曲
             except:
                 print("!!!error to commit " + song_name + "!!!")
                 repeatnumber += 1
-            print()
+            finally:
+                lock.release()
+                print()
         if repeatnumber > 23:
            # con.close()
             return
@@ -119,9 +144,10 @@ def spider_type_chinese(): 		#按类别抓取-华语歌曲
         #con.close()                      #数据库连接关闭
 
 def spider_type_JapAndKor():
-    headers           = {'user-agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36'}
+    #headers           = {'user-agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36'}
 
     for page in range(30):
+        headers = { "User-Agent": random.choice(user_agent)}
         page = str(page)
         japAndkor_url     = "https://www.hifini.com/forum-15-" + page + ".htm?orderby=tid"
         japAndkor_strr    = url_2_strr(japAndkor_url,headers)
@@ -135,6 +161,10 @@ def spider_type_JapAndKor():
             songPic    = get_target(' pic: \'(.*?)\'',song_strr)
 
             if len(songName):           #剔除无效结果
+                pass
+            else:
+                continue
+            if len(songAuthor):
                 pass
             else:
                 continue
@@ -152,7 +182,7 @@ def spider_type_JapAndKor():
             print(songtype)
 
             j = int(i)
-
+            lock.acquire()
             try:                                   #添加异常抛出   执行数据库的数据插入
                 cur.execute("INSERT INTO music_table(music_id,song_name,author,pic_url,type) VALUES(%s,%s,%s,%s,%s)",(j,song_name,song_author,song_pic,songtypenum))
                 con.commit()                  #执行完必须提交
@@ -160,7 +190,10 @@ def spider_type_JapAndKor():
             except:
                 print("!!!error to commit " + song_name + "!!!")
                 repeatnumber += 1
-            print()
+            finally:
+                lock.release()
+                print()
+            
         if repeatnumber > 18:
             
             return
@@ -168,8 +201,9 @@ def spider_type_JapAndKor():
 
 
 def spider_type_eurAndUs():
-    headers = {'user-agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36'}
+    #headers = {'user-agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36'}
     for page in range(30):
+        headers = { "User-Agent": random.choice(user_agent)}
         page = str(page)
         eurAndUs_url     = "https://www.hifini.com/forum-10-" + page + ".htm?orderby=tid"
         eurAndUs_strr    = url_2_strr(eurAndUs_url,headers)
@@ -183,6 +217,10 @@ def spider_type_eurAndUs():
             songPic    = get_target(' pic: \'(.*?)\'',song_strr)
 
             if len(songName):           #剔除无效结果
+                pass
+            else:
+                continue
+            if len(songAuthor):
                 pass
             else:
                 continue
@@ -200,7 +238,7 @@ def spider_type_eurAndUs():
             print(songtype)
 
             j = int(i)
-
+            lock.acquire()
             try:                                   #添加异常抛出   执行数据库的数据插入
                 cur.execute("INSERT INTO music_table(music_id,song_name,author,pic_url,type) VALUES(%s,%s,%s,%s,%s)",(j,song_name,song_author,song_pic,songtypenum))
                 con.commit()                  #执行完必须提交
@@ -208,15 +246,18 @@ def spider_type_eurAndUs():
             except:
                 print("!!!error to commit " + song_name + "!!!")
                 repeatnumber += 1
-            print()
+            finally:
+                lock.release()
+                print()
 
         if repeatnumber > 18:
 
             return
 
 def spider_type_Remix():
-    headers = {'user-agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36'}
+    #headers = {'user-agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36'}
     for page in range(30):
+        headers = { "User-Agent": random.choice(user_agent)}
         page = str(page)
         remix_url     = "https://www.hifini.com/forum-11-" + page + ".htm?orderby=tid"
         remix_strr    = url_2_strr(remix_url,headers)
@@ -230,6 +271,10 @@ def spider_type_Remix():
             songPic    = get_target(' pic: \'(.*?)\'',song_strr)
 
             if len(songName):           #剔除无效结果
+                pass
+            else:
+                continue
+            if len(songAuthor):
                 pass
             else:
                 continue
@@ -247,7 +292,7 @@ def spider_type_Remix():
             print(songtype)
 
             j = int(i)
-
+            lock.acquire()
             try:                                   #添加异常抛出   执行数据库的数据插入
                 cur.execute("INSERT INTO music_table(music_id,song_name,author,pic_url,type) VALUES(%s,%s,%s,%s,%s)",(j,song_name,song_author,song_pic,songtypenum))
                 con.commit()                  #执行完必须提交
@@ -255,15 +300,18 @@ def spider_type_Remix():
             except:
                 print("!!!error to commit " + song_name + "!!!")
                 repeatnumber += 1
-            print()
+            finally:
+                lock.release()
+                print()
             
         if repeatnumber > 18:
 
             return
 
 def spider_type_pure():
-    headers = {'user-agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36'}
+    #headers = {'user-agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36'}
     for page in range(30):
+        headers = { "User-Agent": random.choice(user_agent)}
         page = str(page)
         pure_url     = "https://www.hifini.com/forum-12-" + page + ".htm?orderby=tid"
         pure_strr    = url_2_strr(pure_url,headers)
@@ -277,6 +325,10 @@ def spider_type_pure():
             songPic    = get_target(' pic: \'(.*?)\'',song_strr)
 
             if len(songName):           #剔除无效结果
+                pass
+            else:
+                continue
+            if len(songAuthor):
                 pass
             else:
                 continue
@@ -294,7 +346,7 @@ def spider_type_pure():
             print(songtype)
 
             j = int(i)
-
+            lock.acquire()
             try:                                   #添加异常抛出   执行数据库的数据插入
                 cur.execute("INSERT INTO music_table(music_id,song_name,author,pic_url,type) VALUES(%s,%s,%s,%s,%s)",(j,song_name,song_author,song_pic,songtypenum))
                 con.commit()                  #执行完必须提交
@@ -302,15 +354,18 @@ def spider_type_pure():
             except:
                 print("!!!error to commit " + song_name + "!!!")
                 repeatnumber += 1
-            print()
+            finally:
+                lock.release()
+                print()
             
         if repeatnumber > 18:
 
             return
 
 def spider_type_different():
-    headers = {'user-agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36'}
+    #headers = {'user-agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36'}
     for page in range(30):
+        headers = { "User-Agent": random.choice(user_agent)}
         page = str(page)
         different_url     = "https://www.hifini.com/forum-13-" + page + ".htm?orderby=tid"
         different_strr    = url_2_strr(different_url,headers)
@@ -324,6 +379,10 @@ def spider_type_different():
             songPic    = get_target(' pic: \'(.*?)\'',song_strr)
 
             if len(songName):           #剔除无效结果
+                pass
+            else:
+                continue
+            if len(songAuthor):
                 pass
             else:
                 continue
@@ -341,7 +400,7 @@ def spider_type_different():
             print(songtype)
 
             j = int(i)
-
+            lock.acquire()
             try:                                   #添加异常抛出   执行数据库的数据插入
                 cur.execute("INSERT INTO music_table(music_id,song_name,author,pic_url,type) VALUES(%s,%s,%s,%s,%s)",(j,song_name,song_author,song_pic,songtypenum))
                 con.commit()                  #执行完必须提交
@@ -349,17 +408,118 @@ def spider_type_different():
             except:
                 print("!!!error to commit " + song_name + "!!!")
                 repeatnumber += 1
-            print()
+            finally:
+                lock.release()
+                print()
             
         if repeatnumber > 18:
 
             return
 
+def spidercommon(pages,typename):
+    for page in range(pages):
+        headers = { "User-Agent": random.choice(user_agent)}
+        page = str(page)
+        #common_url = "https://www.hifini.com/forum-13-" + page + ".htm?orderby=tid"
+        common_url = "https://www.hifini.com/forum-" + str(typename.value) + "-" + page + ".htm?orderby=tid"
+        #print(common_url)
+        common_strr = url_2_strr(common_url,headers)
+        #print(common_strr)
+        #return
+        common_songid = get_songid('thread-([1-9][0-9][0-9]*)',common_strr)
+        repeatnumber = 0
+        #print(*common_songid)
+        #print("1111111222222223333333") 
+        for i in common_songid:
+            song_url   = "https://www.hifini.com/thread-" + i + ".htm"
+            song_strr  = url_2_strr(song_url,headers)
+            songName   = get_target(' title: \'(.*?)\',',song_strr)                     #可能出现空songname的情况-有id找不到name--bug
+            songAuthor = get_target(' author:\'(.*?)\',',song_strr)
+            songPic    = get_target(' pic: \'(.*?)\'',song_strr)
+
+            #songtype    = "different"
+            songtypenum = str(typename.value)                        #定义类别协议  ： 华语 = 1   日韩 =  15  欧美 = 10   remix =  11  纯音乐= 12   different = 13
+            song_name   = "".join(songName)
+            song_author = "".join(songAuthor)
+            song_pic    = "".join(songPic)
+
+            if len(song_name):           #剔除无效结果
+                pass
+            else:
+                continue
+            if len(song_author):
+                pass
+            else:
+                continue
+
+            print("ID = " + i)                                #提供调试打印
+            print("".join(songName))
+            print("".join(songAuthor))
+            print("".join(songPic))
+            print(songtypenum)
+
+            j = int(i)
+            
+            lock.acquire()
+            try:                                   #添加异常抛出   执行数据库的数据插入
+                cur.execute("INSERT INTO music_table(music_id,song_name,author,pic_url,type) VALUES(%s,%s,%s,%s,%s)",(j,song_name,song_author,song_pic,songtypenum))
+                con.commit()                  #执行完必须提交
+                #featch = cur.fetchall()
+
+                print("success to commit " + song_name)
+            except Exception as err:
+                print("!!!error to commit " + song_name + "!!!")
+                #print(featch)
+                print("Error %s for sql" % (err))
+                fo = open("log","a") 
+                fo.write(str(err) + song_name + "\n")
+                fo.close()
+                repeatnumber += 1
+            finally:
+                lock.release()
+                
+                print()
+        if repeatnumber > 27:
+
+            return
+
+
+
+
+
+
+
 if __name__ == '__main__':
-    spider_type_chinese()
-    spider_type_JapAndKor()   
-    spider_type_eurAndUs()
-    spider_type_Remix()
-    spider_type_pure()
-    spider_type_different()
+    t1 = threading.Thread(target=spidercommon, args=(10,Typename.CHINESE))
+    t2 = threading.Thread(target=spidercommon, args=(10,Typename.JAPANDKORE))
+    t3 = threading.Thread(target=spidercommon, args=(10,Typename.REMIXE))
+    t4 = threading.Thread(target=spidercommon, args=(10,Typename.EURANDUSE))
+    t5 = threading.Thread(target=spidercommon, args=(10,Typename.PURE))
+    t6 = threading.Thread(target=spidercommon, args=(10,Typename.DIFFERENTE))
+
+    t1.start()
+    t2.start()
+    t3.start()
+    t4.start()
+    t5.start()
+    t6.start()
+
+
+    t1.join()
+    t2.join()
+    t3.join()
+    t4.join()
+    t5.join()
+    t6.join()
+
+    # spider_type_chinese()
+    # spider_type_JapAndKor()   
+    # spider_type_eurAndUs()
+    # spider_type_Remix()
+    # spider_type_pure()
+    # spider_type_different()
+
+    #spidercommon(10,Typename.REMIXE)
+    
     con.close()
+
